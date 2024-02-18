8312d029f1c142ab8cae257ed70f0a414a43d217
Vineet Pradhan <vineetpradhan.v1@gmail.com>
2024-02-18 10:52:32 +0530

    Add Email service in Contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  server: { host: true },
+  integrations: [tailwind(), react()],
+  server: {
+    host: true
+  }
 });
